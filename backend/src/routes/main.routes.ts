10b26dfe8a9681964cf936f76ea64c6757316493
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-11-19 02:01:32 +0800

    refactor(main.routes.ts): update route handlers to use correct controller functions

--- backend/src/routes/main.routes.ts
+++ backend/src/routes/main.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { getMainData } from "../controllers/main.controller.js";
+import { getMain, createMain, updateMain, deleteMain } from "../controllers/main.controller.js";
 
 const router = Router();
 
+
 // GET /api/main
-router.get("/", getMainData);
+router.get("/", getMain);
+// POST /api/main
+router.post("/", createMain);
+// // PUT /api/main/:id
+router.put("/:id", updateMain);
+// DELETE /api/main/:id
+router.delete("/:id", deleteMain);
 
 export default router;
